ff6d9bba00adff2732f984cc437f3f1f18a63951
koshelvadim <koshelvadim@mail.ru>
2023-08-10 14:30:14 +0300

    выполнил 5 ДЗ

--- home5/home5.java
+++ home5/home5.java
@@ -2,57 +2,7 @@ package home5;
 
 import java.util.*;
 
-public class home5 {
-    public static void main(String[] args) {
-        String text = "Это мой первый текст. Он состоит из каких-то тестовых слов и нужен для того, чтобы выполнить тестовое задание GB. " +
-                "Данный текст не несет в себе какого-либо смысла, он просто содержит набор слов.";
-
-        System.out.println(text);
-        printStats(text);
-    }
-
-    static void printStats(String text) {
-        String[] textArray1 = text.toLowerCase().replaceAll("[^а-яА-Яa-zA-z ]", "").split(" ");
-        String[] textArray2 = text.toLowerCase().replaceAll("[^а-яА-Яa-zA-z -]", "").split(" ");
-
-        // создаем массив слов
-        ArrayList<String> arrayList1 = new ArrayList<>(Arrays.asList(textArray1));
-        ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList(textArray2));
-
-        // избавляемся от повторений слов в массиве
-        ArrayList<String> newList1 = new ArrayList<>();
-        ArrayList<String> newList2 = new ArrayList<>();
-
-        for (String element : arrayList1) {
-            if (!newList1.contains(element)) {
-                newList1.add(element);
-            }
-        }
-        for (String element : arrayList2) {
-            if (!newList2.contains(element)) {
-                newList2.add(element);
-            }
-        }
-
-        // создаем коллекцию мап: ключ - количество букв в слове, значение - слова
-        Map<Integer, List<String>> newMap1 = new HashMap<>();
-        Map<Integer, List<String>> newMap2 = new HashMap<>();
-
-        for (String str : newList1) {
-            int key = str.length();
-            newMap1.computeIfAbsent(key, value -> new ArrayList<>()).add(str);
-        }
-        for (String str : newList2) {
-            int key = str.length();
-            newMap2.computeIfAbsent(key, value -> new ArrayList<>()).add(str);
-        }
-
-        System.out.println(newMap1);
-        System.out.println(newMap2);
-    }
-}
-
-/**
+/** ЗАДАЧА
  * Дан текст. Нужно отсортировать слова по длине (по возрастанию) и вывести статистику на экран.
  * Регистр слова не имеет значения. Формат вывода произвольный.
  * Программа-минимум:
@@ -74,3 +24,55 @@ public class home5 {
  * 5 -> [текст, нужен, чтобы, несет, набор]
  * ...
  */
+public class home5 {
+    public static void main(String[] args) {
+        String text = "Это мой первый текст. Он состоит из каких-то тестовых слов и нужен для того, чтобы выполнить тестовое задание GB. " +
+                "Данный текст не несет в себе какого-либо смысла, он просто содержит набор слов.";
+
+        String regex1 = "[^а-яА-Яa-zA-z -]";
+        String regex2 = "[^а-яА-Яa-zA-z ]";
+
+        System.out.println("Исходный текст:\n" + text);
+        String[] textArray1 = textRegs(text, regex1);
+        ArrayList<String> arrayList1 = getNoRepite(textArray1);
+        System.out.println("С учетом дефиса:");
+        getMapCollection(arrayList1);
+
+        String[] textArray2 = textRegs(text, regex2);
+        ArrayList<String> arrayList2 = getNoRepite(textArray2);
+        System.out.println("Без учета дефиса:");
+        getMapCollection(arrayList2);
+    }
+
+    public static String[] textRegs(String text, String regex){
+        String[] textArray = text.toLowerCase().replaceAll(regex, "").split(" ");
+        return textArray;
+    }
+//    public static String[] textRegsWithOutDefise(String text){
+//        String[] textArray = text.toLowerCase().replaceAll("[^а-яА-Яa-zA-z ]", "").split(" ");
+//        return textArray;
+//    }
+
+    public static ArrayList<String> getNoRepite(String[] textArray) {
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(textArray));
+        ArrayList<String> newList = new ArrayList<>();
+
+        for (String element : arrayList) {
+            if (!newList.contains(element)) {
+                newList.add(element);
+            }
+        }
+        return newList;
+    }
+
+    public static void getMapCollection(ArrayList<String> arrayList) {
+        Map<Integer, List<String>> newMap = new HashMap<>();
+
+        for (String str : arrayList) {
+            int key = str.length();
+            newMap.computeIfAbsent(key, value -> new ArrayList<>()).add(str);
+        }
+        System.out.println(newMap);
+    }
+}
+
